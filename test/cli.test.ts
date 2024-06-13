43186010fc77ea236bb83b5c511dc7b2538a8c05
Qz <quadratz@proton.me>
2024-06-13 20:52:04 +0700

    ci: fix ci

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -7,7 +7,8 @@ async function nanoidCli(arg: string = "") {
     cwd: import.meta.dirname,
     args: ["run", "../cli.ts", ...args],
   });
-  const { success, stderr, stdout } = await command.output();
+  const result = await command.output();
+  const { success, stderr, stdout } = result;
   return {
     success,
     stderr: new TextDecoder().decode(stderr),
@@ -17,74 +18,83 @@ async function nanoidCli(arg: string = "") {
 
 Deno.test("CLI", async (t) => {
   await t.step("prints unique ID", async () => {
-    const { stdout, success, stderr } = await nanoidCli();
+    const result = await nanoidCli();
+    const { stdout, success, stderr } = result;
     assertMatch(stdout, /^[\w-]{21}\n$/);
     assertStrictEquals(success, true);
     assertStrictEquals(stderr, "");
   });
 
   await t.step("uses size", async () => {
-    const { stdout, success, stderr } = await nanoidCli("--size 10");
+    const result = await nanoidCli("--size 10");
+    const { stdout, success, stderr } = result;
     assertMatch(stdout, /^[\w-]{10}\n$/);
     assertStrictEquals(success, true);
     assertStrictEquals(stderr, "");
 
-    const { stdout: shortcut } = await nanoidCli("-s 10");
+    const result2 = await nanoidCli("--s 10");
+    const { stdout: shortcut } = result2;
     assertMatch(shortcut, /^[\w-]{10}\n$/);
   });
 
   await t.step("uses alphabet", async () => {
-    const { stdout, success, stderr } = await nanoidCli(
-      "--alphabet abc --size 15",
-    );
+    const result = await nanoidCli("--alphabet abc --size 15");
+    const { stdout, success, stderr } = result;
     assertMatch(stdout, /^[abc]{15}\n$/);
     assertStrictEquals(success, true);
     assertStrictEquals(stderr, "");
 
-    const { stdout: shortcut } = await nanoidCli("-a abc -s 15");
+    const result2 = await nanoidCli("-a abc -s 15");
+    const { stdout: shortcut } = result2;
     assertMatch(shortcut, /^[\w-]{15}\n$/);
   });
 
   await t.step("shows an error on unknown argument", async () => {
-    const { stdout, success, stderr } = await nanoidCli("--cook");
+    const result = await nanoidCli("--cook");
+    const { stdout, success, stderr } = result;
     assertStrictEquals(stdout, "");
     assertStrictEquals(success, false);
     assertMatch(stderr, /.*unexpected argument '--cook'.*/);
   });
 
   await t.step("shows an error if size is not a number", async () => {
-    const { stdout, success, stderr } = await nanoidCli("-s abc");
+    const result = await nanoidCli("-s abc");
+    const { stdout, success, stderr } = result;
     assertStrictEquals(stdout, "");
     assertStrictEquals(success, false);
     assertMatch(stderr, /Invalid size: 'abc'. Please provide a valid number/);
   });
 
   await t.step("shows an error if size is a negative number", async () => {
-    const { stdout, success, stderr } = await nanoidCli("-s=-1");
+    const result = await nanoidCli("-s=-1");
+    const { stdout, success, stderr } = result;
     assertStrictEquals(stdout, "");
     assertStrictEquals(success, false);
     assertMatch(stderr, /Invalid size: '-1'. Please provide a positive number/);
   });
 
   await t.step("displays help", async () => {
-    const { stdout, success, stderr } = await nanoidCli("--help");
+    const result = await nanoidCli("--help");
+    const { stdout, success, stderr } = result;
     assertMatch(stdout, /Show this help/);
     assertStrictEquals(success, true);
     assertStrictEquals(stderr, "");
 
-    const { stdout: shortcut } = await nanoidCli("-h");
+    const result2 = await nanoidCli("-h");
+    const { stdout: shortcut } = result2;
     assertMatch(shortcut, /Show this help/);
   });
 
   await t.step("displays the correct version", async () => {
     const re = new RegExp(denoJson.version);
-
-    const { stdout, success, stderr } = await nanoidCli("--version");
+    const result = await nanoidCli("--version");
+    const { stdout, success, stderr } = result;
     assertMatch(stdout, re);
     assertStrictEquals(success, true);
     assertStrictEquals(stderr, "");
 
-    const { stdout: shortcut } = await nanoidCli("-V");
+    const result2 = await nanoidCli("-V");
+    const { stdout: shortcut } = result2;
     assertMatch(shortcut, re);
   });
 });
